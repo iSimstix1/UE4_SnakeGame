d9631998986347dfed995b97805f9ca907cc17c9
Fedkin Sergey <iSimstix1@mail.ru>
2020-06-08 17:28:53 +0200

    Material_For_Last_Cube

--- Source/SnakeGame/SnakeBase.cpp
+++ Source/SnakeGame/SnakeBase.cpp
@@ -38,7 +38,10 @@ void ASnakeBase::AddSnakeElement(int ElementsNum)
 		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
 		FTransform NewTransform(NewLocation);
 		ASnakeElementBase* NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
-		SnakeElements.Add(NewSnakeElem);
+		int32 ElemIndex = SnakeElements.Add(NewSnakeElem);
+		if (ElemIndex == 0) {
+			NewSnakeElem->SetFirstElementType();
+		}
 	}
 }
 
