3fb5f8383e88c26266977c1d60e2f4bb24969c34
Fedkin Sergey <iSimstix1@mail.ru>
2020-06-08 15:23:45 +0200

    Function_Move()

--- Source/SnakeGame/SnakeBase.cpp
+++ Source/SnakeGame/SnakeBase.cpp
@@ -11,7 +11,8 @@ ASnakeBase::ASnakeBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	ElementSize = 100.f;
-
+	MovementSpeed = 10.f;
+	LastMoveDirection = EMovementDirection::UP;
 }
 
 // Called when the game starts or when spawned
@@ -26,6 +27,7 @@ void ASnakeBase::BeginPlay()
 void ASnakeBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	Move(DeltaTime);
 
 }
 
@@ -34,8 +36,32 @@ void ASnakeBase::AddSnakeElement(int ElementsNum)
 	for (int i = 0; i < ElementsNum; ++i) {
 		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
 		FTransform NewTransform(NewLocation);
-		auto NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		ASnakeElementBase* NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		NewSnakeElem->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
 		SnakeElements.Add(NewSnakeElem);
 	}
+}
+
+void ASnakeBase::Move(float DeltaTime)
+{
+	FVector MovementVector;
+	float MovementSpeedDelta = MovementSpeed * DeltaTime;
+
+	switch (LastMoveDirection) {
+	case EMovementDirection::UP:
+		MovementVector.X += MovementSpeedDelta;
+		break;
+	case EMovementDirection::DOWN:
+		MovementVector.X -= MovementSpeedDelta;
+		break;
+	case EMovementDirection::RIGHT:
+		MovementVector.Y += MovementSpeedDelta;
+		break;
+	case EMovementDirection::LEFT:
+		MovementVector.Y -= MovementSpeedDelta;
+		break;
+	}
+
+	AddActorWorldOffset(MovementVector);
 }
 
--- Source/SnakeGame/SnakeBase.h
+++ Source/SnakeGame/SnakeBase.h
@@ -8,6 +8,15 @@
 
 class ASnakeElementBase;
 
+UENUM()
+enum class EMovementDirection 
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
 UCLASS()
 class SNAKEGAME_API ASnakeBase : public AActor
 {
@@ -23,8 +32,14 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 		float ElementSize;
 
+	UPROPERTY(EditDefaultsOnly)
+		float MovementSpeed;
+
 	UPROPERTY()
 		TArray<ASnakeElementBase*> SnakeElements;
+
+	UPROPERTY()
+		EMovementDirection LastMoveDirection;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -37,4 +52,5 @@ public:
 
 	void AddSnakeElement(int ElementsNum = 1);
 
+	void Move(float DeltaTime);
 };
